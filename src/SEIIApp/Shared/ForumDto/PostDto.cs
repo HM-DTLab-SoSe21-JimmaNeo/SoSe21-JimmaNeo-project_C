f327a55d28c9288716038408ad0ae82c029e7966
Bumblebee1993 <T\^:q!Do;OE=r=1>
2021-05-27 14:16:37 +0200

    Logo / Links

---
+++ src/SEIIApp/Shared/ForumDto/PostDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SEIIApp.Shared
+{
+    public class PostDto
+    {
+            
+        [Key]
+        public int PostId { get; set; }
+
+        public int UserId { get; set; }
+
+        [Required]
+        public bool IsAuthorized { get; set; }
+
+        public string Title { get; set; }
+        public DateTime UploadDate { get; set; }
+        public DateTime LastChange { get; set; }
+        public string Content { get; set; }
+        public byte[] Attachment { get; set; }
+        public int Likes { get; set; }
+        public int CommentCounter { get; set; }
+  
+    }
+}
