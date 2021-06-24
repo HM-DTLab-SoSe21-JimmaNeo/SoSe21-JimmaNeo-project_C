6f15694d57ad1a10b7d53c24dfc8a58189c707d4
Daniel Bronold <bronold@hm.edu>
2021-06-24 22:33:00 +0200

    Final commit

--- eNeoAuth/eNeoAuth/Client/obj/Debug/net5.0/RazorDeclaration/Shared/LoginDisplay.razor.g.cs
+++ eNeoAuth/eNeoAuth/Client/obj/Debug/net5.0/RazorDeclaration/Shared/LoginDisplay.razor.g.cs
@@ -13,98 +13,98 @@ namespace eNeoAuth.Client.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Daniel\source\repos\eNeoAuth\eNeoAuth\Client\_Imports.razor"
+#line 1 "C:\Users\Daniel\Documents\Studium\4. Semester\Software Engineering II\project_c\eNeoAuth\eNeoAuth\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Daniel\source\repos\eNeoAuth\eNeoAuth\Client\_Imports.razor"
+#line 2 "C:\Users\Daniel\Documents\Studium\4. Semester\Software Engineering II\project_c\eNeoAuth\eNeoAuth\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Daniel\source\repos\eNeoAuth\eNeoAuth\Client\_Imports.razor"
+#line 4 "C:\Users\Daniel\Documents\Studium\4. Semester\Software Engineering II\project_c\eNeoAuth\eNeoAuth\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Daniel\source\repos\eNeoAuth\eNeoAuth\Client\_Imports.razor"
+#line 5 "C:\Users\Daniel\Documents\Studium\4. Semester\Software Engineering II\project_c\eNeoAuth\eNeoAuth\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Daniel\source\repos\eNeoAuth\eNeoAuth\Client\_Imports.razor"
+#line 6 "C:\Users\Daniel\Documents\Studium\4. Semester\Software Engineering II\project_c\eNeoAuth\eNeoAuth\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Daniel\source\repos\eNeoAuth\eNeoAuth\Client\_Imports.razor"
+#line 7 "C:\Users\Daniel\Documents\Studium\4. Semester\Software Engineering II\project_c\eNeoAuth\eNeoAuth\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Daniel\source\repos\eNeoAuth\eNeoAuth\Client\_Imports.razor"
+#line 8 "C:\Users\Daniel\Documents\Studium\4. Semester\Software Engineering II\project_c\eNeoAuth\eNeoAuth\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Daniel\source\repos\eNeoAuth\eNeoAuth\Client\_Imports.razor"
+#line 9 "C:\Users\Daniel\Documents\Studium\4. Semester\Software Engineering II\project_c\eNeoAuth\eNeoAuth\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Daniel\source\repos\eNeoAuth\eNeoAuth\Client\_Imports.razor"
+#line 10 "C:\Users\Daniel\Documents\Studium\4. Semester\Software Engineering II\project_c\eNeoAuth\eNeoAuth\Client\_Imports.razor"
 using eNeoAuth.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\Daniel\source\repos\eNeoAuth\eNeoAuth\Client\_Imports.razor"
+#line 11 "C:\Users\Daniel\Documents\Studium\4. Semester\Software Engineering II\project_c\eNeoAuth\eNeoAuth\Client\_Imports.razor"
 using eNeoAuth.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\Daniel\source\repos\eNeoAuth\eNeoAuth\Client\_Imports.razor"
+#line 12 "C:\Users\Daniel\Documents\Studium\4. Semester\Software Engineering II\project_c\eNeoAuth\eNeoAuth\Client\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "C:\Users\Daniel\source\repos\eNeoAuth\eNeoAuth\Client\_Imports.razor"
+#line 13 "C:\Users\Daniel\Documents\Studium\4. Semester\Software Engineering II\project_c\eNeoAuth\eNeoAuth\Client\_Imports.razor"
 using System.Security.Claims;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 1 "C:\Users\Daniel\source\repos\eNeoAuth\eNeoAuth\Client\Shared\LoginDisplay.razor"
+#line 1 "C:\Users\Daniel\Documents\Studium\4. Semester\Software Engineering II\project_c\eNeoAuth\eNeoAuth\Client\Shared\LoginDisplay.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Daniel\source\repos\eNeoAuth\eNeoAuth\Client\Shared\LoginDisplay.razor"
+#line 2 "C:\Users\Daniel\Documents\Studium\4. Semester\Software Engineering II\project_c\eNeoAuth\eNeoAuth\Client\Shared\LoginDisplay.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 #line default
@@ -118,7 +118,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 18 "C:\Users\Daniel\source\repos\eNeoAuth\eNeoAuth\Client\Shared\LoginDisplay.razor"
+#line 18 "C:\Users\Daniel\Documents\Studium\4. Semester\Software Engineering II\project_c\eNeoAuth\eNeoAuth\Client\Shared\LoginDisplay.razor"
       
     private async Task BeginSignOut(MouseEventArgs args)
     {
